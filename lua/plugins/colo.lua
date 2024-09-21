c9a14600cdd7a8965653ca47464845c295c9c93c
SalmonPest <staaf.elis@gmail.com>
2024-09-21 17:19:27 +0200

    Update colo.lua

--- lua/plugins/colo.lua
+++ lua/plugins/colo.lua
@@ -3,12 +3,7 @@
 -- add them here.
 
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-    },
-  }
+  { "folke/tokyonight.nvim" },
+  { "olimorris/onedarkpro.nvim" },
+  { "Mofiqul/dracula.nvim" },
+}
