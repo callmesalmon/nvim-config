8b1240401470fd68c3f61db19c032f21cc16d796
SalmonPest <staaf.elis@gmail.com>
2024-09-21 18:07:49 +0200

    Create colo.lua

---
+++ lua/plugins/colo.lua
@@ -0,0 +1,11 @@
+-- == Colorschemes ==
+--[[ 
+File for colorschemes,
+add them here. 
+]]
+
+return {
+  { "folke/tokyonight.nvim" },
+  { "olimorris/onedarkpro.nvim" },
+  { "Mofiqul/dracula.nvim" },
+}
